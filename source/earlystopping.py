929e06d550f24b7f6f10882bb87a162759b72a71
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-03 13:38:40 +0530

    Update earlystopping.py

--- source/earlystopping.py
+++ source/earlystopping.py
@@ -16,3 +16,74 @@ from sklearn.model_selection import train_test_split
 # accuracy_score to calculate the accuracy of predictions
 from sklearn.metrics import accuracy_score
 
+# reading the csv file
+data = pd.read_csv('data/Dataset/emergency_classification.csv')
+
+print(data.head())
+
+# create random number generator
+seed = 42
+rng = np.random.RandomState(seed)
+
+# load images and store it in numpy array
+# empty list to store the images
+X = []
+# iterating over each image
+for img_name in data.image_names:
+    # loading the image using its name
+    img = plt.imread('data/Dataset/images/' + img_name)
+    # saving each image in the list
+    X.append(img)
+
+# converting the list of images into array
+X=np.array(X)
+
+# storing the target variable in separate variable
+y = data.emergency_or_not.values
+
+
+# shape of the images
+print(X.shape)
+
+## 2. Pre-processing the data
+# converting 3 dimensional image to 1 dimensional image
+X = X.reshape(X.shape[0], 224*224*3)
+print(X.shape)
+
+# minimum and maximum pixel values of images
+X.min(), X.max()
+
+print(X.min(), X.max())
+
+# normalizing the pixel values
+X = X / X.max()
+
+# minimum and maximum pixel values of images after normalizing
+X.min(), X.max()
+
+print(X.min(), X.max())
+
+## 3. Creating training and validation set
+# creating a training and validation set
+X_train, X_valid, y_train, y_valid=train_test_split(X,y,test_size=0.3, random_state=seed)
+
+# shape of training and validation set
+(X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape)
+
+
+## 4. Defining the model architecture
+# defining the model architecture
+model=Sequential()
+
+model.add(InputLayer(input_shape=(224*224*3,)))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(units=1, activation='sigmoid'))
+from keras.optimizers import Adam
+# compiling the model\
+# defining the adam optimizer and setting the learning rate as 10^-5
+adam = Adam(lr=1e-5)
+# Compile the model and specify the optimizer (Adam) correctly
+model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+
