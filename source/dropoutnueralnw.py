6852f6282f7b402c8be9916f1f95674720f1debc
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-03 16:52:41 +0530

    adding dropout for improving deep learning model

--- source/dropoutnueralnw.py
+++ source/dropoutnueralnw.py
@@ -33,7 +33,7 @@ X = []
 # iterating over each image
 for img_name in data.image_names:
     # loading the image using its name
-    img = plt.imread('Dataset/images/' + img_name)
+    img = plt.imread('data/Dataset/images/' + img_name)
     # saving each image in the list
     X.append(img)
  
@@ -49,6 +49,64 @@ y = data.emergency_or_not.values
 X = X.reshape(X.shape[0], 224*224*3)
 
 print(X.shape)
+
+# normalizing the pixel values
+X = X / X.max()
+
+# minimum and maximum pixel values of images after normalizing
+X.min(), X.max()
+
+print(X.min(), X.max())
+
+
+## 3. Creating training and validation set
+# creating a training and validation set
+X_train, X_valid, y_train, y_valid=train_test_split(X,y,test_size=0.3, random_state=seed)
+
+# shape of training and validation set
+(X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape)
+
+print((X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape))
+
+## 4. Defining the model architecture
+### <ol>Adding dropout layer(s)</ol>
+# importing the dropout layer
+from keras.layers import Dropout
+
+# defining the model architecture with dropout layer
+model=Sequential()
+
+model.add(InputLayer(input_shape=(224*224*3,)))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(units=1, activation='sigmoid'))
+
+## 5. Compiling the model
+
+# defining the adam optimizer and setting the learning rate as 10^-5
+adam = Adam(lr=1e-5)
+
+# compiling the model
+
+# defining loss as binary crossentropy
+# defining optimizer as Adam
+# defining metrics as accuracy
+
+model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+## 6. Training the model using Dropout
+# training the model for 100 epochs
+
+model_history = model.fit(X_train, y_train, epochs=100, batch_size=128,validation_data=(X_valid,y_valid))
+
+## 7. Evaluating model performance 
+# accuracy on validation set
+# Corrected code:
+predictions = model.predict(X_valid)
+predictions_classes = (predictions > 0.5).astype(int)  # Convert probabilities to binary class labels
+print('Accuracy on validation set:', accuracy_score(y_valid, predictions_classes) * 100, '%')
 
 
 
