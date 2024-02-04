d2dd3115d2b49f50d919cb2c89ad1946572ec5a2
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-04 11:11:42 +0530

    Create weightinitialization.py

---
+++ source/weightinitialization.py
@@ -0,0 +1,13 @@
+# Steps to solve Emergency vs Non-Emergency vehicle classification
+#<ol>1. Loading the dataset</ol>
+'''
+<ol>2. Pre-processing the data</ol>
+<ol>3. Creating training and validation set</ol>
+<ol>4. Defining the model architecture</ol>
+<ol><ol>Setting up the weight initialization technique</ol></ol>
+<ol>5. Compiling the model</ol>
+<ol>6. Training the model</ol>
+<ol>7. Evaluating model performance</ol>
+
+'''
+
