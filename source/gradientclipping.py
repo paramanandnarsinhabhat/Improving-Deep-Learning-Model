3c053c45709f2d3258de150af91b1dd3bce68738
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-04 10:14:59 +0530

    new file created for vanishing and exploding gradients

--- source/gradientclipping.py
+++ source/gradientclipping.py
@@ -1,11 +1,10 @@
-### Steps to solve Emergency vs Non-Emergency vehicle classification problem using Dropout
+# Gradient Clipping in Keras
+#<ol>1. Loading the dataset</ol>
 ## 1. Loading the dataset
 # import necessary libraries and functions
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 
 # importing layers from keras
 from keras.layers import Dense, InputLayer
@@ -18,6 +17,7 @@ from sklearn.model_selection import train_test_split
 # accuracy_score to calculate the accuracy of predictions
 from sklearn.metrics import accuracy_score
 
+
 # reading the csv file
 data = pd.read_csv('data/Dataset/emergency_classification.csv')
 
@@ -28,6 +28,7 @@ seed = 42
 rng = np.random.RandomState(seed)
 
 # load images and store it in numpy array
+
 # empty list to store the images
 X = []
 # iterating over each image
@@ -36,8 +37,7 @@ for img_name in data.image_names:
     img = plt.imread('data/Dataset/images/' + img_name)
     # saving each image in the list
     X.append(img)
- 
-     
+    
 # converting the list of images into array
 X=np.array(X)
 
@@ -47,7 +47,7 @@ y = data.emergency_or_not.values
 ## 2. Pre-processing the data
 # converting 3 dimensional image to 1 dimensional image
 X = X.reshape(X.shape[0], 224*224*3)
-
+X.shape
 print(X.shape)
 
 # normalizing the pixel values
@@ -57,7 +57,6 @@ X = X / X.max()
 X.min(), X.max()
 
 print(X.min(), X.max())
-
 
 ## 3. Creating training and validation set
 # creating a training and validation set
@@ -69,24 +68,17 @@ X_train, X_valid, y_train, y_valid=train_test_split(X,y,test_size=0.3, random_st
 print((X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape))
 
 ## 4. Defining the model architecture
-### <ol>Adding dropout layer(s)</ol>
-# importing the dropout layer
-from keras.layers import Dropout
-
 # defining the model architecture with dropout layer
 model=Sequential()
 
 model.add(InputLayer(input_shape=(224*224*3,)))
 model.add(Dense(100, activation='sigmoid'))
-model.add(Dropout(rate=0.5))
 model.add(Dense(100, activation='sigmoid'))
-model.add(Dropout(rate=0.5))
 model.add(Dense(units=1, activation='sigmoid'))
 
 ## 5. Compiling the model
-
-# defining the adam optimizer and setting the learning rate as 10^-5
-adam = Adam(lr=1e-5)
+# defining the adam optimizer and setting the learning rate as 10^-5 and adding clip value to clip the gradients
+adam = Adam(lr=1e-5, clipvalue=1)
 
 # compiling the model
 
@@ -96,25 +88,7 @@ adam = Adam(lr=1e-5)
 
 model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
-## 6. Training the model using Dropout
+## 6. Training the model
 # training the model for 100 epochs
 
 model_history = model.fit(X_train, y_train, epochs=100, batch_size=128,validation_data=(X_valid,y_valid))
-
-## 7. Evaluating model performance 
-# accuracy on validation set
-# Corrected code:
-predictions = model.predict(X_valid)
-predictions_classes = (predictions > 0.5).astype(int)  # Convert probabilities to binary class labels
-print('Accuracy on validation set:', accuracy_score(y_valid, predictions_classes) * 100, '%')
-
-# summarize history for loss
-plt.plot(model_history.history['loss'])
-plt.plot(model_history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper right')
-plt.show()
-
-
